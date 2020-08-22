bd724f30156d5e65aead745c14f422c863a49795
aewens <email@aewens.com>
2020-08-22 16:24:16 +0000

    Replaced logging with panic to control where logging occurs

--- pkg/reading/config.go
+++ pkg/reading/config.go
@@ -2,7 +2,7 @@ package reading
 
 import (
 	"encoding/json"
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/aewens/anda/pkg/core"
@@ -11,7 +11,6 @@ import (
 func Config(path string) (*core.Config, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
 
@@ -20,78 +19,84 @@ func Config(path string) (*core.Config, error) {
 	raw := make(map[string]interface{})
 	err = json.NewDecoder(file).Decode(&raw)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
 
 	rawApiPort, ok := raw["port"]
 	if !ok {
-		log.Fatalf("Missing 'port' from config file: %s", path)
+		panic(fmt.Sprintf("Missing 'port' from config file: %s", path))
 	}
 
 	apiPort, valid := rawApiPort.(float64)
 	if !valid {
-		log.Fatalf("Value of 'port' is not a number: %v", apiPort)
+		panic(fmt.Sprintf("Value of 'port' is not a number: %v", apiPort))
 	}
 
 	rawDatabase, ok := raw["database"]
 	if !ok {
-		log.Fatalf("Missing 'database' from config file: %s", path)
+		panic(fmt.Sprintf("Missing 'database' from config file: %s", path))
 	}
 
 	database, valid := rawDatabase.(map[string]interface{})
 	if !valid {
-		log.Fatalf("Value of 'port' is not a dictionary: %v", rawDatabase)
+		panic(fmt.Sprintf("Value of 'port' is not a dictionary: %v",
+			rawDatabase))
 	}
 
 	rawHost, ok := database["host"]
 	if !ok {
-		log.Fatalf("Missing 'host' from database in config file: %s", path)
+		panic(fmt.Sprintf("Missing 'host' from database in config file: %s",
+			path))
 	}
 
 	host, valid := rawHost.(string)
 	if !valid {
-		log.Fatalf("Value of 'host' is not a string: %v", rawHost)
+		panic(fmt.Sprintf("Value of 'host' is not a string: %v", rawHost))
 	}
 
 	rawPort, ok := database["port"]
 	if !ok {
-		log.Fatalf("Missing 'port' from database in config file: %s", path)
+		panic(fmt.Sprintf("Missing 'port' from database in config file: %s",
+			path))
 	}
 
 	port, valid := rawPort.(string)
 	if !valid {
-		log.Fatalf("Value of 'port' is not a string: %v", rawPort)
+		panic(fmt.Sprintf("Value of 'port' is not a string: %v", rawPort))
 	}
 
 	rawUser, ok := database["user"]
 	if !ok {
-		log.Fatalf("Missing 'user' from database in config file: %s", path)
+		panic(fmt.Sprintf("Missing 'user' from database in config file: %s",
+			path))
 	}
 
 	user, valid := rawUser.(string)
 	if !valid {
-		log.Fatalf("Value of 'user' is not a string: %v", rawUser)
+		panic(fmt.Sprintf("Value of 'user' is not a string: %v", rawUser))
 	}
 
 	rawPassword, ok := database["password"]
 	if !ok {
-		log.Fatalf("Missing 'password' from database in config file: %s", path)
+		panic(fmt.Sprintf("Missing 'password' from database in config file: %s",
+			path))
 	}
 
 	password, valid := rawPassword.(string)
 	if !valid {
-		log.Fatalf("Value of 'password' is not a string: %v", rawPassword)
+		panic(fmt.Sprintf("Value of 'password' is not a string: %v",
+			rawPassword))
 	}
 
 	rawName, ok := database["name"]
 	if !ok {
-		log.Fatalf("Missing 'name' from database in config file: %s", path)
+		panic(fmt.Sprintf("Missing 'name' from database in config file: %s",
+			path))
 	}
 
 	name, valid := rawName.(string)
 	if !valid {
-		log.Fatalf("Value of 'name' is not a string: %v", rawUser)
+		panic(fmt.Sprintf("Value of 'name' is not a string: %v", rawUser))
 	}
 
 	config := &core.Config{
